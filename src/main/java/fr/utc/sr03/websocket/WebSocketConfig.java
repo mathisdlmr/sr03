183522f376ff729d91e959833847b43986b2f6fa
mathisDlmr <mathis.dlmr@etik.com>
2026-06-12 16:35:11 +0200

    fix(backend: websocket): create a ServletServerContainerFactoryBean to support image upload on websocket

--- src/main/java/fr/utc/sr03/websocket/WebSocketConfig.java
+++ src/main/java/fr/utc/sr03/websocket/WebSocketConfig.java
@@ -6,10 +6,12 @@ import fr.utc.sr03.services.InvitationService;
 import fr.utc.sr03.services.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
@@ -28,11 +30,24 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Value("${spring.websocket.max-text-message-size:5242880}")
     private int maxTextMessageSize;
 
+    @Value("${spring.websocket.max-binary-message-size:5242880}")
+    private int maxBinaryMessageSize;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(
             new WebSocketHandler(jwtUtil, userService, chatService, invitationService), "/ws/chat/*")
             .setAllowedOrigins("*")
             .addInterceptors(new HttpSessionHandshakeInterceptor());
+    }
+
+    // On définit un nouveau container WebSocket pour override le container WebSocket Tomcat embarqué qui a une limite à 8ko
+    // https://tomcat.apache.org/tomcat-9.0-doc/web-socket-howto.html (bien trop petit pour supporter l'upload d'une image)
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(maxTextMessageSize);
+        container.setMaxBinaryMessageBufferSize(maxBinaryMessageSize);
+        return container;
     }
 }
