b56e463ff9f8227048a89e3aef811e27fa1011b8
mathisDlmr <mathis.dlmr@etik.com>
2026-06-14 11:24:04 +0200

    feat(code quality): setup prettierc

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,10 @@
+// https://medium.com/@crbodarya05/data-how-to-set-up-prettier-in-a-react-app-the-right-way-f5fe45d40bf4
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
